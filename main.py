dd47baf0b7c3872b409170896f36032cd7c976d3
michalwisnia <michalmanu@gmail.com>
2022-03-30 14:45:47 +0200

    finding contact pages and emails

--- main.py
+++ main.py
@@ -1,32 +1,132 @@
+import urllib
+
 import pandas as pd
-import numpy as np
-from csv import DictReader
 import requests
+from bs4 import BeautifulSoup
+import re
 
-def url_checker(url: object) -> object:
-	try:
-		#Get Url
-		get = requests.get(url)
-		# if the request succeeds
-		if get.status_code == 200:
-			return(f"{url}: is reachable")
-		else:
-			return(f"{url}: is Not reachable, status_code: {get.status_code}")
+def scrap_emails(link):
+    response = requests.get(link)
+    soup = BeautifulSoup(response.content, "html.parser")
+    mailtos = soup.select('a[href^=mailto]')
+    emails = []
+    for i in mailtos:
+        #if i.string != None:
+            #emails.append(i.string.encode('utf-8').strip())
+        href = i['href']
+        try:
+            str1, str2 = href.split(':')
+        except ValueError:
+            break
+        emails.append(str2)
 
-	#Exception
-	except requests.exceptions.RequestException as e:
-        #print URL with Errs
-		print(e)
+    return emails
+dtypes = {
+    'Kod_TERYT': 'object', 'nazwa_samorządu': 'object', 'Województwo': 'object', 'Powiat': 'object', 'typ_JST': 'object',
+    'nazwa_urzędu_JST': 'object', 'miejscowość': 'object', 'Kod pocztowy': 'object', 'poczta': 'object','Ulica': 'object',
+    'Nr domu': 'object', 'telefon kierunkowy': 'object', 'telefon': 'object', 'telefon 2': 'object', 'wewnętrzny': 'object',
+    'FAX kierunkowy': 'object', 'FAX': 'object', 'FAX wewnętrzny': 'object',
+    'ogólny adres poczty elektronicznej gminy/powiatu/województwa': 'object', 'adres www jednostki': 'object',
+    'ESP': 'object'
+}
 
-if __name__ == '__main__':
-    with open('csv_Baza_teleadresowa_jst_stan_na_19_05_2021.csv', 'r') as read_obj:
-        csv_dict_reader = DictReader(read_obj)
-    df = pd.read_csv('csv_Baza_teleadresowa_jst_stan_na_19_05_2021.csv', encoding = 'unicode_escape', sep=';', lineterminator='\r')
-    #for col_name in df.columns:
-        #print(col_name)
-    for adres in df["adres www jednostki"]:
-	    if not str(adres).startswith("http://"):
-	        adres = "http://" + adres
-	    print(url_checker(adres))
+baza_teleadresowa_jst_df = pd.read_csv("csv_Baza_teleadresowa_jst_stan_na_19_05_2021.csv", sep=";", encoding="windows-1250", dtype=dtypes)
+baza_teleadresowa_jst_df = baza_teleadresowa_jst_df.loc[:, ~baza_teleadresowa_jst_df.columns.str.contains('^Unnamed')]
 
 
+i = 0
+for adres_www in baza_teleadresowa_jst_df['adres www jednostki']:
+    kontakt_url = None
+
+    if not adres_www.startswith("http"):
+        url = "http://" + adres_www
+        print(str(i) + " " + url)
+        try:
+            response = requests.get(url)
+            # print(response.status_code)
+            soup = BeautifulSoup(response.content, "html.parser")
+
+            if soup.select_one("a[href*=bip]") is not None:
+                a = soup.select_one("a[href*=bip]")
+                href = a.get('href')
+                # print(href)
+                if href.startswith("http"):
+                    kontakt_url = href
+                    print(kontakt_url)
+                else:
+                    kontakt_url = url + href
+                    print(kontakt_url)
+            elif soup.select_one("a[href*=biuletyn]") is not None:
+                a = soup.select_one("a[href*=biuletyn]")
+                href = a.get('href')
+                # print(href)
+                if href.startswith("http"):
+                    kontakt_url = href
+                    print(kontakt_url)
+                else:
+                    kontakt_url = url + href
+                    print(kontakt_url)
+            else:
+                print("test1")
+
+            if kontakt_url is not None:
+                kontakt_url_response = requests.get(kontakt_url)
+                print(kontakt_url_response.status_code)
+        except requests.exceptions.RequestException as e:
+            try:
+                new_url = e.request.url
+                print(new_url)
+                response = requests.get(new_url, verify=False)
+                # print(response.status_code)
+                soup = BeautifulSoup(response.content, "html.parser")
+                if soup.select_one("a[href*=bip]") is not None:
+                    a = soup.select_one("a[href*=bip]")
+                    href = a.get('href')
+                    # print(href)
+                    if href.startswith("http"):
+                        kontakt_url = href
+                        print(kontakt_url)
+                    else:
+                        kontakt_url = new_url + href
+                        print(kontakt_url)
+                    kontakt_url_response = requests.get(kontakt_url)
+                    print(kontakt_url_response.status_code)
+            except requests.exceptions.RequestException as e:
+                print("blad")
+                # continue
+
+    else:
+        url = adres_www
+        print(str(i) + " " + url)
+        try:
+            response = requests.get(url)
+            # print(response.status_code)
+            soup = BeautifulSoup(response.content, "html.parser")
+
+            if soup.select_one("a[href*=bip]") is not None:
+                a = soup.select_one("a[href*=bip]")
+                href = a.get('href')
+                # print(href)
+                if href.startswith("http"):
+                    kontakt_url = href
+                    print(kontakt_url)
+                else:
+                    if href.startswith("/pl") and url.endswith("pl/"):
+                        kontakt_url = url[:-3] + href
+                    else:
+                        kontakt_url = url + href
+                    print(kontakt_url)
+            else:
+                print("test2")
+
+            if kontakt_url is not None:
+                kontakt_response = requests.get(kontakt_url)
+                print(kontakt_response.status_code)
+        except requests.exceptions.RequestException as e:
+            print("blad")
+            continue
+
+    print(scrap_emails(kontakt_url))
+
+
+    print()
